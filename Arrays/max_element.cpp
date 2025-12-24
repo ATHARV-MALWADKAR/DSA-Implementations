cd61a84c95cefff1f5b49d1712857e4212c5b903
Atharv <atharvmalwadkar17@gmail.com>
2025-12-24 23:04:34 +0530

    Create max_element.cpp

---
+++ Arrays/max_element.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int mx = arr[0];
+    for(int i = 1; i < n; i++) {
+        mx = max(mx, arr[i]);
+    }
+
+    cout << mx << endl;
+    return 0;
+}
